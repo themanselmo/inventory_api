38f95dace840755747bf9fd94c5ef82531f6d1d4
max <itmanselmo@gmail.com>
2024-12-01 01:58:40 -0500

    add description to products and tailwind styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_01_012017) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_01_040807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_01_012017) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
